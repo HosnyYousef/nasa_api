eea71bf22c171c6f89ab3f7e300a9256951fd854
Yousef Hosny <Yousefhosny.hm@gmail.com>
2024-12-25 15:28:45 -0800

    trying to get the API title explination and img working

--- js/main.js
+++ js/main.js
@@ -3,15 +3,15 @@
 document.querySelector('button').addEventListener('click', getDrink)
 
 function getDrink(){
-    let drink = (document.querySelector('input').value)
-
-    fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drink)
+    const choice = document.querySelector('input').value.toLowerCase()
+    const url = `https://api.nasa.gov/planetary/apod?api_key=gGIh2JmUXbLcG7fwadwnbaaZg2klIH4P3GWrYnbP`
+fetch(url)
     .then(res => res.json()) // parse response as JSON
     .then(data => {
-      console.log(data.drinks)
-      document.querySelector('h2').innerText = data.drinks[0].strDrink
-      document.querySelector('img').src = data.drinks[0].strDrinkThumb
-      document.querySelector('h3').innerText = data.drinks[0].strInstructions
+      console.log(data)
+      document.querySelector('h2').innerText = data.title
+      document.querySelector('img').src = data.hdurl
+      document.querySelector('h3').innerText = data.explanation
     })
     .catch(err => {
         console.log(`error ${err}`)
