aa8ad58a7da86764d088b534afc40f1c5dbbf41e
Yousef Hosny <Yousefhosny.hm@gmail.com>
2024-12-26 14:08:58 -0800

    created some questions to review later

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
 
-window.onbeforeunload = function(event)
-{
-    return confirm("Confirm refresh");
-};
+// window.onbeforeunload = function(event)
+// {
+//     return confirm("Confirm refresh");
+// };
 
 document.querySelector('button').addEventListener('click', getDrink)
 
@@ -38,8 +38,47 @@ fetch(url)
 // e.g.: `https://api.nasa.gov/planetary/apod?api_key=gGIh2JmUXbLcG7fwadwnbaaZg2klIH4P3GWrYnbP&date=${choice}`
 
 
+// QUESTON:
 // how do we change code to accept video?
 
+// function getDrink(){
+//   const choice = document.querySelector('input').value
+//   const url = `https://api.nasa.gov/planetary/apod?api_key=gGIh2JmUXbLcG7fwadwnbaaZg2klIH4P3GWrYnbP&date=${choice}`
+// fetch(url)
+//   .then(res => res.json()) // parse response as JSON
+//   .then(data => {
+//     console.log(data)
+//     document.querySelector('h2').innerText = data.title
+//     document.querySelector('img').src = data.url
+//     document.querySelector('iframe').src = data.url
+//     document.querySelector('h3').innerText = data.explanation
+//     // media type
+//   })
+//   .catch(err => {
+//       console.log(`error ${err}`)
+//   });
+// }
 
+// ANSWER:
+// function getDrink(){
+//   const choice = document.querySelector('input').value
+//   const url = `https://api.nasa.gov/planetary/apod?api_key=gGIh2JmUXbLcG7fwadwnbaaZg2klIH4P3GWrYnbP&date=${choice}`
+// fetch(url)
+//   .then(res => res.json()) // parse response as JSON
+//   .then(data => {
+//     console.log(data)
+//     document.querySelector('h2').innerText = data.title
+//     if (data.media_type === 'image') {
+//       document.querySelector('img').src = data.url
+//     }else if(data.media_type === 'video'){
+//       document.querySelector('iframe').src = data.url
+//     }
+//     document.querySelector('h3').innerText = data.explanation
+//     // media type
+//   })
+//   .catch(err => {
+//       console.log(`error ${err}`)
+//   });
+// }
 
 //www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic
