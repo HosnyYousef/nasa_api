332daeb67be48491d6c05ad98fc76c2766ff01d3
HosnyYousef <yousefhosny.hm@gmail.com>
2024-12-25 16:27:49 -0800

    worked with photos and vides now

--- js/main.js
+++ js/main.js
@@ -10,8 +10,13 @@ fetch(url)
     .then(data => {
       console.log(data)
       document.querySelector('h2').innerText = data.title
-      document.querySelector('img').src = data.hdurl
+      if (data.media_type === 'image') {
+        document.querySelector('img').src = data.hdurl
+      }else if(data.media_type === 'video'){
+        document.querySelector('iframe').src = data.url
+      }
       document.querySelector('h3').innerText = data.explanation
+      // media type
     })
     .catch(err => {
         console.log(`error ${err}`)
